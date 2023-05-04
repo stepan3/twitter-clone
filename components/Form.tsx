0c24e496229f4e0142bf7d6ec310bbfd41efa111
Stepan Jamalyan <stepan.jamalyan@gmail.com>
2023-05-04 17:03:43 +0600

    added comments

--- components/Form.tsx
+++ components/Form.tsx
@@ -2,13 +2,14 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { toast } from "react-hot-toast";
 
-import useCurrentUser from "@/hooks/useCurrentUser";
 import useLoginModal from "@/hooks/useLoginModal";
-import usePosts from "@/hooks/usePosts";
 import useRegisterModal from "@/hooks/useRegisterModal";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import usePosts from "@/hooks/usePosts";
+import usePost from "@/hooks/usePost";
 
-import Button from "./Button";
 import Avatar from "./Avatar";
+import Button from "./Button";
 
 interface FormProps {
   placeholder: string;
@@ -22,6 +23,7 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
 
   const { data: currentUser } = useCurrentUser();
   const { mutate: mutatePosts } = usePosts();
+  const { mutate: mutatePost } = usePost(postId as string);
 
   const [body, setBody] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -30,18 +32,20 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
     try {
       setIsLoading(true);
 
-      await axios.post("/api/posts", { body });
+      const url = isComment ? `/api/comments?postId=${postId}` : "/api/posts";
 
-      toast.success("Tweet Created");
+      await axios.post(url, { body });
 
+      toast.success("Tweet created");
       setBody("");
       mutatePosts();
+      mutatePost();
     } catch (error) {
       toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
     }
-  }, [body, mutatePosts]);
+  }, [body, mutatePosts, isComment, postId, mutatePost]);
 
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
@@ -53,32 +57,31 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
           <div className="w-full">
             <textarea
               disabled={isLoading}
-              onChange={(e) => setBody(e.target.value)}
+              onChange={(event) => setBody(event.target.value)}
               value={body}
               className="
                 disabled:opacity-80
                 peer
-                resize-none
-                mt-3
-                w-full
-                bg-black
-                ring-0
-                outline-none
-                text-[20px]
-                placeholder-neutral-500
+                resize-none 
+                mt-3 
+                w-full 
+                bg-black 
+                ring-0 
+                outline-none 
+                text-[20px] 
+                placeholder-neutral-500 
                 text-white
               "
               placeholder={placeholder}
             ></textarea>
             <hr
               className="
-                opacity-0
-                peer-focus:opacity-100
-                h-[1px]
-                w-full
-                border-neutral-800
-                transition
-            "
+                opacity-0 
+                peer-focus:opacity-100 
+                h-[1px] 
+                w-full 
+                border-neutral-800 
+                transition"
             />
             <div className="mt-4 flex flex-row justify-end">
               <Button
@@ -91,15 +94,7 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
         </div>
       ) : (
         <div className="py-8">
-          <h1
-            className="
-            text-white
-            text-2xl
-            text-center
-            mb-4
-            font-bold
-        "
-          >
+          <h1 className="text-white text-2xl text-center mb-4 font-bold">
             Welcome to Twitter
           </h1>
           <div className="flex flex-row items-center justify-center gap-4">
