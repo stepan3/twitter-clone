099525582b293829c6de5b02c6e51d5a57274ec2
Stepan Jamalyan <stepan.jamalyan@gmail.com>
2023-05-04 14:01:51 +0600

    added creating and loading posts

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,6 +1,6 @@
-import { useCallback } from "react";
-import { useRouter } from "next/router";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
 
 import useUser from "@/hooks/useUser";
 
@@ -12,6 +12,7 @@ interface AvatarProps {
 
 const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
   const router = useRouter();
+
   const { data: fetchedUser } = useUser(userId);
 
   const onClick = useCallback(
@@ -29,14 +30,14 @@ const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
     <div
       className={`
         ${hasBorder ? "border-4 border-black" : ""}
-        ${isLarge ? "h-32" : "h12"}
+        ${isLarge ? "h-32" : "h-12"}
         ${isLarge ? "w-32" : "w-12"}
-        rounded-full
-        hover:opacity-90
-        transition
+        rounded-full 
+        hover:opacity-90 
+        transition 
         cursor-pointer
         relative
-    `}
+      `}
     >
       <Image
         fill
